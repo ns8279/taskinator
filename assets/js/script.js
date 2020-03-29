e369b529e6cf296b31c5a54f40abd5c95f54b44a
Nirupama Sundararaman <nirupama.2308@gmail.com>
2020-03-29 10:51:47 -0700

    adding event listener

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+var buttonEl = document.querySelector("#save-task");
+var taskToDoEl = document.querySelector("#tasks-to-do");
+
+buttonEl.addEventListener("click", function(){
+    var listItemEl = document.createElement("li");
+    listItemEl.className = "task-item";
+    listItemEl.textContent = "This is a new task";
+    taskToDoEl.appendChild(listItemEl);
+
+})
