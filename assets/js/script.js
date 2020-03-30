82566e378e1d2a3f9c3e21c84a856bc5fdd04e44
Nirupama Sundararaman <nirupama.2308@gmail.com>
2020-03-30 11:38:39 -0700

    Commit before the branch switch-no updates

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,3 @@
 var buttonEl = document.querySelector("#save-task");
 var taskToDoEl = document.querySelector("#tasks-to-do");
 
-buttonEl.addEventListener("click", function(){
-    var listItemEl = document.createElement("li");
-    listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task";
-    taskToDoEl.appendChild(listItemEl);
-
-})
